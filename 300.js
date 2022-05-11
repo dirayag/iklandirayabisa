35fef2c2421b53fb5dc43cd6b55206aa12026e31
dirayag <95455839+dirayag@users.noreply.github.com>
2022-05-11 19:20:13 +0700

    Create 300.js

---
+++ 300.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '1b62451882492590b0f3836e0264c783',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplayformats.com/1b62451882492590b0f3836e0264c783/invoke.js"></scr' + 'ipt>');
