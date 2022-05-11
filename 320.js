edcdc0bf1e5773a14bbc39075c9bb16aa8c3c379
dirayag <95455839+dirayag@users.noreply.github.com>
2022-05-11 19:22:19 +0700

    Create 320.js

---
+++ 320.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '783787640b99d4d7358b407f916aa55c',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplayformats.com/783787640b99d4d7358b407f916aa55c/invoke.js"></scr' + 'ipt>');
